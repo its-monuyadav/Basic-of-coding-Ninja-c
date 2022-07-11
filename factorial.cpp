06061ba6cc1259e97397473fc6655285db9482e2
Monu <kumarmonu1812@gmail.com>
2022-07-12 01:53:15 +0530

    https://github.com/its-monuyadav/Basic-of-coding-Ninja-c.git

---
+++ factorial.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number, factorial = 1;
+
+    cout << "Enter a number :" << endl;
+    cin >> number;
+
+    for (int i = 1; i <= number; i++)
+    {
+        factorial *= i;
+    }
+
+    cout << "Factorial : " << factorial << endl;
+
+    return 0;
+}
